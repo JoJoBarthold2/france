0e3f667a358c2512d975441261d3048d382dd800
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-12-16 23:10:34 +0100

    clown

--- gregtorevo.js
+++ gregtorevo.js
@@ -43,15 +43,22 @@ function validInput(string){
 
 function convertDate(input){
     var parts = dateString.split("/");
-    var day = parseInt(parts[1], 10);
-    var month = parseInt(parts[0], 10);
+    var greg_day = parseInt(parts[1], 10);
+    var greg_month = parseInt(parts[0], 10);
     var year = parseInt(parts[2], 10);
     var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
     const french_months = ["Vendémiaire", "Brumaire", "Frimaire", "Nivôse", "Pluviôse", "Ventôse", "Germinal", "Floréal", "Prairial", "Messidor", "Thermidor", "Fructidor"];
     const french_days = ["Primidi", "Duodi", "Tridi", "Quartidi", "Quintidi", "Sextidi", "Septidi", "Octidi", "Nonidi", "Decadi"];
     if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
      monthLength[1] = 29;
-    
 
+     var french_year = year - 1789;
+    
+     var day_in_year =0;
+     for (let month = 0; month < greg_month; month++) {
+        day_in_year += monthLength[month];
+    }
+    day_in_year += greg_day;
+    
 
 }
