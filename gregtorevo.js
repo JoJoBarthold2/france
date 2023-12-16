25b27aabbf71c1d4f664f91b89f1f6e9bf458252
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-12-16 22:21:30 +0100

    start to insult users

--- gregtorevo.js
+++ gregtorevo.js
@@ -1,5 +1,25 @@
 function displayInput() {
     var userInput = document.getElementById("userInput").value;
-    
-    document.getElementById("output").innerText = "User Input: " + userInput;
+    if (validInput(userInput))
+    { 
+        var convertedInput = convertInput(userInput);
+        var output = computeDate(convertedInput);
+      
+    }
+    else{
+        output = "Invalid Input. Fuck yourself";
+    }
+    document.getElementById("output").innerText = "User Input: " + output;
+}
+
+function validInput(string){
+return false;
+}
+
+function convertInput(validInput){
+
+}
+
+function computeDate(convertInput){
+
 }
